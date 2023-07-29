6143a4184a77e50abb3d4be2aeaef25691738601
Joshua Hoban <joshua.hoban@countersight.co>
2023-07-29 18:37:15 +0200

    add head shot to page

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography, useTheme } from "@mui/material";
+import head_shot from "~/images/me/headshot.jpg";
 
 const Intro = () => {
   const theme = useTheme();
@@ -19,6 +20,28 @@ const Intro = () => {
         <Typography variant="subtitle1" color="#FFF">
           "Yeet, Yoat, Yeetus"
         </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            width: "333px",
+            height: "181px",
+            padding: "0px 76px",
+            justifyContent: "center",
+            alignItems: "center",
+            flexShrink: 0,
+          }}
+        >
+          <img
+            src={head_shot}
+            style={{
+              width: "181px",
+              height: "181px",
+              flexShrink: 0,
+              borderRadius: "6.678px",
+              border: "2.003px solid #000",
+            }}
+          />
+        </Box>
       </Box>
     </Box>
   );
