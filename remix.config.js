389efbecb9eafd8c388ac55055da4914c74dcc0d
Joshua Hoban <joshua.hoban@countersight.co>
2023-07-31 11:55:29 +0200

    add netlify files from template

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.ts"
+      : undefined,
+  serverBuildPath: ".netlify/functions-internal/server.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   serverModuleFormat: "cjs",
   future: {
