f9963232eb3725b6aad119bcaafc66351bfd4671
Joshua Hoban <joshua.hoban@countersight.co>
2023-08-02 16:57:39 +0200

    remove some images

--- app/utils/images.server.ts
+++ app/utils/images.server.ts
@@ -1,7 +1,6 @@
 import image_1 from "~/images/me/image_1.jpg"
 import image_2 from "~/images/me/image_2.jpg"
 import image_3 from "~/images/me/image_3.jpg"
-import image_4 from "~/images/me/image_4.jpg"
 import image_5 from "~/images/me/image_5.jpg"
 import image_6 from "~/images/me/image_6.jpg"
 import image_7 from "~/images/me/image_7.jpg"
@@ -9,14 +8,12 @@ import image_8 from "~/images/me/image_8.jpg"
 import image_9 from "~/images/me/image_9.jpg"
 import image_10 from "~/images/me/image_10.jpg"
 import image_11 from "~/images/me/image_11.jpg"
-import image_12 from "~/images/me/image_12.jpg"
 
 //temporary solution to storing images in a serverless backend environment.
 export const image_list = [
   image_1,
   image_2,
   image_3,
-  image_4,
   image_5,
   image_6,
   image_7,
@@ -24,5 +21,4 @@ export const image_list = [
   image_9,
   image_10,
   image_11,
-  image_12,
 ]
