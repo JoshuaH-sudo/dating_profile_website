f53b1b8d8ffb20b5946de788de047915faf9ab48
Joshua Hoban <joshua.hoban@countersight.co>
2023-08-04 16:10:51 +0200

    add fart noise when transitioning from home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -34,6 +34,7 @@ export default function Splash_page() {
           height: "100vh",
           objectFit: "cover",
         }}
+        disablePictureInPicture
         autoPlay
         loop
         id="gigachad-video"
--- app/routes/home._index.tsx
+++ app/routes/home._index.tsx
@@ -4,14 +4,36 @@ import Divider from "~/components/UI/Divider";
 import Intro from "~/components/sections/Intro";
 import home_page_image from "~/images/layouts/home-background-image.png";
 import { useRef } from "react";
+import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import fart_noise from "~/assets/audio/quick-reverb-fart.mp3";
+
+export const loader: LoaderFunction = ({ request }) => {
+  const referer = request.headers.get("referer");
+  if (!referer) return false;
+
+  const base_url = new URL(request.url).origin + "/";
+  return base_url === referer;
+};
 
 export default function Index_route() {
   const scroll_element_ref = useRef<Element>(null);
+  const from_home_page = useLoaderData() as Boolean;
 
-  const execute_scroll = () => scroll_element_ref?.current?.scrollIntoView({ behavior: "smooth"});
+  const execute_scroll = () =>
+    scroll_element_ref?.current?.scrollIntoView({ behavior: "smooth" });
 
   return (
     <>
+      {from_home_page && (
+        <audio
+          style={{ position: "fixed", height: 0, visibility: "hidden" }}
+          controls
+          autoPlay
+        >
+          <source src={fart_noise} type="audio/mp3" />
+        </audio>
+      )}
       <Box
         style={{
           height: "100%",
@@ -46,7 +68,7 @@ export default function Index_route() {
             height: "100%",
           }}
         >
-          <Intro execute_scroll={execute_scroll}/>
+          <Intro execute_scroll={execute_scroll} />
         </Box>
         <Divider />
       </Box>
